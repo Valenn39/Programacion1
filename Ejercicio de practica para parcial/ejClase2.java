85c1fe283fb6af4badd9d3974e351cd759e40037
Valenn39 <valenolass39@gmail.com>
2026-04-28 20:15:14 -0300

    Simulacro ej3

--- Ejercicio de practica para parcial/ejClase2.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class ejClase2 {
-    public static void main( String [] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Ingrese el alto: ");
-    int alto = sc.nextInt;
-    }
-}
---
+++ EjercicioDePracticaParaParcial/SistemaDeCalificacion/estudiante.java
@@ -0,0 +1,11 @@
+package EjercicioDePracticaParaParcial.SistemaDeCalificacion;
+
+public class estudiante {
+    
+    String nombre;
+    Float nota; 
+
+    public static void main (String[] args){
+        promedioEstudiante = promedio 
+    }
+}
